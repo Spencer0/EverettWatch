ac8ff4d80785fdacefb8ade77ab7d1a2c4381028
EverettWatch <compusa001@yahoo.com>
2020-02-01 17:08:55 -0800

    added direct user to data table implementation

--- scripts/eventHandlers.js
+++ scripts/eventHandlers.js
@@ -249,16 +249,43 @@ function dataInputFormSubmission(){
 function buildInputObject(){
   var userSelection = {
     name: "",
-    age: 0,
-    height: 0,
-    gender: ""
+    datereceived: "",
+    title: "Title",
+    incidenttype: "",
+    location: "",
+    description: ""
   }
 
+  // Find a <table> element with id="myTable":
+  var table = document.getElementById("datatable");
+
+  // Create an empty <tr> element and add it to the 1st position of the table:
+  var row = table.insertRow(1);
+
+  // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+  var cell0 = row.insertCell(0);
+  var cell1 = row.insertCell(1);
+  var cell2 = row.insertCell(2);
+  var cell3 = row.insertCell(3);
+  var cell4 = row.insertCell(4);
+  var cell5 = row.insertCell(5);
+
   userSelection.name = document.getElementById("name").value;
-  userSelection.age = document.getElementById("age").value;
-  userSelection.height = document.getElementById("height").value;
-  userSelection.gender = document.getElementById("gender").value;
+  userSelection.datereceived = document.getElementById("datereceived").value;
+  userSelection.title = document.getElementById("title").value;
+  userSelection.incidenttype = document.getElementById("incidenttype").value;
+  userSelection.location = document.getElementById("location").value;
+  userSelection.description = document.getElementById("description").value;
   console.log("Created user input object", userSelection)
+
+  // Add some text to the new cells:
+  cell0.innerHTML = userSelection.name;
+  cell1.innerHTML = userSelection.datereceived;
+  cell2.innerHTML = userSelection.title;
+  cell3.innerHTML = userSelection.incidenttype;
+  cell4.innerHTML = userSelection.location;
+  cell5.innerHTML = userSelection.description;
+
   return userSelection;
 }
 
